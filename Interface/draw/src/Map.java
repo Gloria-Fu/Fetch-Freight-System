1ad36ed27d44cd21d650bed6039f67011ba9b5ed
Fu Shiqing <73346348+Gloria-Fu@users.noreply.github.com>
2020-11-10 22:03:46 +0800

    update interface

--- Interface/draw/src/Map.java
+++ Interface/draw/src/Map.java
@@ -25,27 +25,15 @@ public class Map extends Thread
                 super.paint(g);
 
                 try {
-                    drawRobot(g);
+                    drawMap(g);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
 
-                drawTarget(g);
+                drawConveyor(g);
 
                 try {
                     drawRobots(g);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    showInfo(g);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    showInfo2(g);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
@@ -78,7 +66,7 @@ public class Map extends Thread
 
 
 
-    public static int drawRobot(Graphics g) throws FileNotFoundException {
+    public static int drawMap(Graphics g) throws FileNotFoundException {
 
         String path = "E:\\project\\3X3V6\\video\\V0\\map.txt";
 
@@ -124,143 +112,7 @@ public class Map extends Thread
         return 0;
     }
 
-
-    public static int showInfo(Graphics g) throws FileNotFoundException {
-
-        String path = "E:\\project\\3X3V6\\video\\V0\\news.txt";
-
-        Scanner in = new Scanner(new File(path));
-        List data = new ArrayList();
-
-        while (in.hasNextLine()) {
-            int num = in.nextInt();
-            if (num == -1) {
-                break;
-            }
-            int[] arrs = {num};
-            data.add(arrs);
-        }
-
-        g.setColor(Color.black);
-        Font font = new Font("Times New Roman", Font.PLAIN, 18);
-        g.setFont(font);
-        int airsX = (width / 10) * gap;
-        int airsY = 15;
-        g.drawString(" Shenzhen Institute of Artificial Intelligence and Robotics\n" +
-                " for Society  ----   Fetch & Freight Robots Cooperatively Scheduling Simulation Platform", airsX, airsY);
-
-        g.setColor(Color.BLACK);
-        font = new Font("Times New Roman", Font.BOLD, 15);
-        g.setFont(font);
-
-        int textX = (width + 1) * gap;
-        int textY = 4 * gap;
-        int gapp = 22;
-
-        int[] a = new int[3];
-        for (int i = 0; i < data.size(); i++) {
-            int[] arr = (int[]) data.get(i);
-            a[i] = arr[0];
-        }
-        g.drawString("Fetch Robot Number: 110", textX, textY);
-        g.drawString("Freight Robot Number: 220", textX, textY += gapp);
-        g.drawString("Goods Area Number: 36", textX, textY += gapp);
-        g.drawString("Goods Type Number: 40", textX, textY += gapp);
-        g.drawString("The Current Working Time Step: " + String.valueOf(a[0]), textX, textY += gapp);
-        g.drawString("The Total Task Number: " + String.valueOf(a[1]), textX, textY += gapp);
-        g.drawString("The Finished Task Number: " + String.valueOf(a[2]), textX, textY += gapp);
-
-        font = new Font("Times New Roman", Font.BOLD, 13);
-        g.setColor(Color.red);
-        g.fillRect(100, 920, gridSize + 2, gridSize + 2);
-        g.setColor(Color.BLACK);
-        g.drawString("Goods Shelf", 135, 930);
-
-        g.setColor(Color.ORANGE);
-        g.fillRect(100 + gridSize * 20, 920, gridSize + 2, gridSize + 2);
-        g.setColor(Color.BLACK);
-        g.drawString("Goods Area", 135 + gridSize * 20, 930);
-
-        g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(100 + gridSize * 40, 920, gridSize + 2, gridSize + 2);
-        g.setColor(Color.BLACK);
-        g.drawString("Conveyor", 135 + gridSize * 40, 930);
-
-        g.setColor(Color.BLACK);
-        g.fillRect(100 + gridSize * 60, 920, gridSize + 4, gridSize + 4);
-        g.setColor(Color.white);
-        g.fillRect(100 + gridSize * 60 + 1, 921, gridSize + 2, gridSize + 2);
-        g.setColor(Color.BLACK);
-        g.drawString("Public Road", 135 + gridSize * 60, 930);
-
-        g.setColor(Color.MAGENTA);
-        g.fillOval(100 + gridSize * 80, 925, 3, 4);
-        g.fillOval(100 + gridSize * 80+6, 925, 3, 4);
-        g.fillRect(100 + gridSize * 80, 922, gridSize + 2, gridSize / 2);
-        g.setColor(Color.BLACK);
-        g.drawString("The Freight Robot", 135 + gridSize * 80, 930);
-
-        g.setColor(Color.blue);
-        g.fillRect(100 + gridSize * 100,920 + gridSize - 5, gridSize, 5);
-        g.fillRect(100 + gridSize * 100 + gridSize/2 - 2, 920, 2, gridSize - 5);
-        int [] xPoints = new int [] {100 + gridSize * 100 + gridSize/2, 100 + gridSize * 100 + gridSize/2 + 3, 100 + gridSize * 100 + gridSize/2 + 5};
-        int [] yPoints = new int [] {920, 920 + 2, 920 + 2};
-        g.drawPolyline(xPoints, yPoints, 3);
-        g.drawLine(100 + gridSize * 100 + gridSize/2 + 3, 920 + 3, 100 + gridSize * 100 + gridSize/2 + 5, 920 + 1);
-        g.drawLine(100 + gridSize * 100 + gridSize/2 + 3, 920 + 3, 100 + gridSize * 100 + gridSize/2 + 5, 920 + 5);
-        g.setColor(Color.BLACK);
-        g.drawString("The Fetch Robot", 135 + gridSize * 100, 930);
-
-        return 0;
-    }
-
-
-
-    public static int showInfo2(Graphics g) throws FileNotFoundException {
-
-        String path = "E:\\project\\3X3V6\\video\\V0\\news2.txt";
-
-        Scanner in = new Scanner(new File(path));
-        List data = new ArrayList();
-
-        while (in.hasNextLine()) {
-            int num = in.nextInt();
-            if(num == -1){
-                break;
-            }
-            int[] arrs = {num};
-            data.add(arrs);
-        }
-
-        g.setColor(Color.BLACK);
-        Font font = new Font("Times New Roman", Font.BOLD, 14);
-        g.setFont(font);
-
-        int textX = (width+1)*gap;
-        int textY = 22*gap;
-        int gapp = 18;
-
-        int[] a = new int[5];
-        int j = 0;
-        for (int i = 0; i < data.size(); i++) {
-            int[] arr = (int[]) data.get(i);
-            a[j] = arr[0];
-            j++;
-            if(j >= 5){
-                j = 0;
-                if(a[0] == 1){
-                    g.drawString("Fetch Robot "+String.valueOf(a[2])+" and Freight Robot "+String.valueOf((a[3]))+" handover successfully.",textX,textY+=gapp);
-                }else if(a[0] == 2){
-                    g.drawString("Task "+String.valueOf(a[1])+" has been finished successfully.",textX,textY+=gapp);
-                }
-            }
-        }
-
-        return 0;
-    }
-
-
-    public static int drawTarget(Graphics g) {
+    public static int drawConveyor(Graphics g) {
 
         g.setColor(Color.LIGHT_GRAY);
 
